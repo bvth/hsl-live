4af05a02d8d8b80fce359c1dc274ed1b635d6854
Thanh Bui <thanh@identio.fi>
2023-08-03 12:12:53 +0300

    decode gtfs data

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,18 @@
-import * as protobuf from "protobufjs";
+import GtfsRealtimeBindings from "gtfs-realtime-bindings";
 
 const getData = async () => {
-  const res = await fetch("https://data.waltti.fi/tampere/api/gtfsrealtime/v1.0/feed/servicealert", {
-    headers: {
-      "Authorization": `Basic ${btoa(`${process.env.WALTTI_CLIENT_ID}:${process.env.WALTTI_CLIENT_SECRET}`)}`
-    }
-  })
-
-  return protobuf.load("gtfs-realtime.proto").then( root => {
-    let AlertMessage = root.lookupType('transit_realtime.Alert')
-    let payload = res.body;
-    let errMsg = AlertMessage.verify(payload);
-    if (errMsg)
-      throw Error(errMsg);
-
-    let msg = AlertMessage.create(payload)
-
-    let buffer = AlertMessage.encode(msg).finish();
-    // ... do something with buffer
-
-    // Decode an Uint8Array (browser) or Buffer (node) to a message
-    let message = AlertMessage.decode(buffer);
-
-    console.log('======', message)
-    return AlertMessage.toObject(message)
+  fetch("https://realtime.hsl.fi/realtime/service-alerts/v2/hsl").then(res => res).then(async res => {
+    const buffer = await res.arrayBuffer();
+    let feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(buffer));
+    feed.entity.forEach((entity) => {
+      if (entity.alert) {
+        console.log(entity.alert.descriptionText);
+      }
+    });
   })
 }
 
 export default async function Home() {
   const data = await getData();
-  console.log('----....--', data)
   return <p>hello</p>
 }
